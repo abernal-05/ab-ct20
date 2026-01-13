30fcfe906fa1669aba721b35785503f9798172a4
abernal-05 <arantxabernal71@gmail.com>
2026-01-12 18:16:51 -0800

    weeklyyyy

---
+++ 01826/p5js-in-class/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+   console.log("Canvas Created");
+}
+
+function draw() {
+  background('#F3CFF5');
+  stroke('#CF54E5');
+  fill('#E31B9D');
+  circle(200,200,50);
+  strokeWeight(1);
+  fill('#B4DAE3');
+  triangle(200,200,200,100,100,100);
+  
+}
