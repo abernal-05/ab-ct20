41886a18fbd9badb6f32190964da3c9dcbb98665
abernal-05 <arantxabernal71@gmail.com>
2026-01-22 13:41:36 -0800

    week2

---
+++ 012026/hexagonal-rocket/sketch.js
@@ -0,0 +1,19 @@
+let img;
+function preload(){
+  img = loadImage("https://raw.githubusercontent.com/abernal-05/ab-ct20/refs/heads/main/kiki.png")
+  console.log("Kiki loaded")
+}
+
+function setup(){
+  createCanvas(4000, 4000);
+}
+
+function draw() {
+  background("#DFB9D8");
+  image(img,mouseX,mouseY,200,200)
+  fill("#5C3D57")
+  strokeWeight(6)
+  stroke("#B18FAB") 
+  text('Kiki!',mouseX,mouseY,1000);
+  noCursor()
+}
